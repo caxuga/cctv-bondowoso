a693deeaa3b77a6dedace5c733e9e213a736a8ab
Caxuga <soegapixel@gmail.com>
2025-06-18 21:13:24 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Halaman1() {
         </p>
 
         {/* HIT COUNTER */}
-          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fcctv-bondowoso.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=eye.svg&icon_color=%23FFFFFF&title=hits&edge_flat=false"/>
+          <img src="https://hits.sh/cctv-bondowoso.vercel.app.svg?style=flat-square&label=visitor&color=blue&labelColor=gray" />
       </div>
       {/* END OF FOOTER */}
     </div>
