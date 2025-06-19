780bec61a54fd474b150091c1f9ee294f34e3818
Caxuga <soegapixel@gmail.com>
2025-06-19 09:48:37 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -49,11 +49,22 @@ export default function Halaman1() {
 <hr className="mt-10 mb-4 border-t border-gray-700" />
 
       {/* FOOTER */}
-<p>
-<center>
+
+<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,1509227,4,4006,112,61,00011101']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?1509227&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+        
 <img src="https://hits.sh/cctv-bondowoso.vercel.app.svg?style=flat-square&label=visitor&color=blue&labelColor=gray" />
-</center>
-</p>
 <hr>
       <div className="mt-10 text-center text-sm text-gray-400">
         by{' '}
