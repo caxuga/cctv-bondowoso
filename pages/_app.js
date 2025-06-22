8b0dcead303d9b5ba58219b78a1bcb02c5c1ded4
caxode <soegapixel@gmail.com>
2025-06-22 20:06:18 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://embed.tawk.to/68561b05c44c79190f9881b6/1iu85j53s";
-    script.async = true;
-    script.charset = "UTF-8";
-    script.setAttribute("crossorigin", "*");
-    document.body.appendChild(script);
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.src = "https://embed.tawk.to/68561b05c44c79190f9881b6/1iu85j53s";
+      script.async = true;
+      script.charset = "UTF-8";
+      script.setAttribute("crossorigin", "*");
+      document.body.appendChild(script);
+    }
   }, []);
 
   return <Component {...pageProps} />;
