535ed3221695173d0258e0ff962945e6205add9c
caxode <soegapixel@gmail.com>
2025-06-22 20:03:57 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to/68561b05c44c79190f9881b6/1iu85j53s";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+  }, []);
+
   return <Component {...pageProps} />;
 }
